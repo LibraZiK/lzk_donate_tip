6607ef8d9c63d20ca877e6b2bc899608a8a4e978
Olivier Humbert <trebmuh@users.noreply.github.com>
2020-10-19 20:00:28 +0200

    Frenc translation

--- locale/lzk_donatetip_fr_FR.ts
+++ locale/lzk_donatetip_fr_FR.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../resources/ui/donate.ui" line="14"/>
         <source>Give For Librazik </source>
-        <translation>Donnez Pour Librazik </translation>
+        <translation>Donnez pour LibraZiK </translation>
     </message>
     <message>
         <location filename="../resources/ui/donate.ui" line="28"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Thanks to use LIBRAZIK !&lt;/p&gt;&lt;p&gt;You seems to be a regular LIBRAZIK user.&lt;br/&gt;Create and maintain this distribution is a hard work that takes time.&lt;br/&gt;That is why donations (even small) are really appreciated.&lt;/p&gt;&lt;p&gt;You can donate &lt;a href=&quot;https://liberapay.com/LibraZiK/donate&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#2980b9;&quot;&gt;here&lt;/span&gt;&lt;/a&gt; .&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Merci d&apos;utiliser LIBRAZIK !&lt;/p&gt;&lt;p&gt;Il semble que vous soyez un utilisateur régulier.&lt;br/&gt;Créer et maintenir cette distribution est un gros travail qui prend du temps.&lt;br/&gt;C&apos;est pourquoi les donations (mêmes petites) sont fortement appréciées.&lt;/p&gt;&lt;p&gt;Vous pouvez donner  &lt;a href=&quot;https://liberapay.com/LibraZiK/donate&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#2980b9;&quot;&gt;ici&lt;/span&gt;&lt;/a&gt; .&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Merci d&apos;utiliser source !&lt;/p&gt;&lt;p&gt;Il semble que vous soyez un utilisateur régulier.&lt;br/&gt;Créer et maintenir cette distribution est un gros travail qui prend du temps.&lt;br/&gt;C&apos;est pourquoi les donations (mêmes petites) sont fortement appréciées.&lt;/p&gt;&lt;p&gt;Vous pouvez donner  &lt;a href=&quot;https://liberapay.com/LibraZiK/donate&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#2980b9;&quot;&gt;ici&lt;/span&gt;&lt;/a&gt; .&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../resources/ui/donate.ui" line="87"/>
@@ -21,12 +21,12 @@
     <message>
         <location filename="../resources/ui/donate.ui" line="59"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:696;&quot;&gt;Tip Of The Day&lt;br/&gt;________________&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:696;&quot;&gt;Astuce Du Jour&lt;br/&gt;________________&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:696;&quot;&gt;Astuce uu jour&lt;br/&gt;________________&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../resources/ui/donate.ui" line="69"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Do you know &lt;span style=&quot; font-weight:600;&quot;&gt;ALT+F2&lt;/span&gt; shortcut ?&lt;/p&gt;&lt;p&gt;&lt;br/&gt;This shortcut allows you to start very quicky&lt;br/&gt;any software by typing its name.&lt;/p&gt;&lt;p&gt;This way, you don&apos;t have to create many and many&lt;br/&gt;shortcuts on the desktop.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Connaissez-vous le raccourci &lt;span style=&quot; font-weight:600;&quot;&gt;ALT+F2&lt;/span&gt; ?&lt;/p&gt;&lt;p&gt;&lt;br/&gt;Ce raccorci permet de démarrer très rapidement&lt;br/&gt;n&apos;importe quel programme en tapant son nom.&lt;/p&gt;&lt;p&gt;Comme ça, vous n&apos;avez pas à créer pléthore&lt;br/&gt;de raccourcis sur votre bureau.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Connaissez-vous le raccourci &lt;span style=&quot; font-weight:600;&quot;&gt;ALT+F2&lt;/span&gt; ?&lt;/p&gt;&lt;p&gt;&lt;br/&gt;Ce raccourci permet de démarrer très rapidement&lt;br/&gt;n&apos;importe quel programme en tapant son nom.&lt;/p&gt;&lt;p&gt;Comme ça, vous n&apos;avez pas à créer pléthore&lt;br/&gt;de raccourcis sur votre bureau.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
 </context>
 <context>
@@ -34,12 +34,12 @@
     <message>
         <location filename="../src/lzk_donate_tip.py" line="34"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Do you know &lt;span style=&quot; font-weight:600;&quot;&gt;ALT+F2&lt;/span&gt; shortcut ?&lt;/p&gt;&lt;p&gt;&lt;br/&gt;This shortcut allows you to start very quicky&lt;br/&gt;any software by typing its name.&lt;/p&gt;&lt;p&gt;This way, you don&apos;t have to create many and many&lt;br/&gt;shortcuts on the desktop.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Connaissez-vous le raccourci &lt;span style=&quot; font-weight:600;&quot;&gt;ALT+F2&lt;/span&gt; ?&lt;/p&gt;&lt;p&gt;&lt;br/&gt;Ce raccorci permet de démarrer très rapidement&lt;br/&gt;n&apos;importe quel programme en tapant son nom.&lt;/p&gt;&lt;p&gt;Comme ça, vous n&apos;avez pas à créer pléthore&lt;br/&gt;de raccourcis sur votre bureau.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Connaissez-vous le raccourci &lt;span style=&quot; font-weight:600;&quot;&gt;ALT+F2&lt;/span&gt; ?&lt;/p&gt;&lt;p&gt;&lt;br/&gt;Ce raccuorci permet de démarrer très rapidement&lt;br/&gt;n&apos;importe quel programme en tapant son nom.&lt;/p&gt;&lt;p&gt;Comme ça, vous n&apos;avez pas à créer pléthore&lt;br/&gt;de raccourcis sur votre bureau.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../src/lzk_donate_tip.py" line="36"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Do you know &lt;span style=&quot; font-weight:600;&quot;&gt;ALT+Tab&lt;/span&gt; shortcut ?&lt;/p&gt;&lt;p&gt;&lt;br/&gt;This shortcut allows you to switch quickly&lt;br/&gt;between windows.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Connaissez-vous le raccourci &lt;span style=&quot; font-weight:600;&quot;&gt;ALT+Tab&lt;/span&gt; ?&lt;/p&gt;&lt;p&gt;&lt;br/&gt;Ce raccourci vous permet de basculer rapidement &lt;br/&gt;entre les fenêtres.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
 </context>
 </TS>
