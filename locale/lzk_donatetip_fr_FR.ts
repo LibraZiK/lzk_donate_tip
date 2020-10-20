6cb62cb31f48e4f6383e536455a59f6b3c0023ba
Olivier Humbert <trebmuh@users.noreply.github.com>
2020-10-20 23:48:31 +0200

    Màj textes & affichage fenêtre (#7)
    * mise à jour fichier UI
    * MàJ de la trad FR
    * MàJ du fichier EN

--- locale/lzk_donatetip_fr_FR.ts
+++ locale/lzk_donatetip_fr_FR.ts
@@ -9,35 +9,45 @@
         <translation>Donnez à LibraZiK</translation>
     </message>
     <message>
-        <location filename="../resources/ui/donate.ui" line="87"/>
+        <location filename="../resources/ui/donate.ui" line="185"/>
         <source>Do not show this message again</source>
         <translation>Ne plus afficher ce message</translation>
     </message>
     <message>
-        <location filename="../resources/ui/donate.ui" line="59"/>
+        <location filename="../resources/ui/donate.ui" line="109"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:696;&quot;&gt;Tip Of The Day&lt;br/&gt;________________&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:696;&quot;&gt;Astuce du jour&lt;br/&gt;________________&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../resources/ui/donate.ui" line="69"/>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Do you know the &lt;span style=&quot; font-weight:600;&quot;&gt;ALT+F2&lt;/span&gt; shortcut?&lt;/p&gt;&lt;p&gt;This shortcut allows you to start any software&lt;br/&gt;very quickly by simply typing its name.&lt;/p&gt;&lt;p&gt;This way you don&apos;t have to create many and&lt;br/&gt;many shortcuts on the desktop.&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Connaissez-vous le raccourci &lt;span style=&quot; font-weight:600;&quot;&gt;ALT+F2&lt;/span&gt; ?&lt;/p&gt;&lt;p&gt;Ce raccourci permet de démarrer très rapidement&lt;br/&gt;n&apos;importe quel programme en tapant son nom.&lt;/p&gt;&lt;p&gt;Comme ça, vous n&apos;avez pas à créer pléthore&lt;br/&gt;de raccourcis sur votre bureau.&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <location filename="../resources/ui/donate.ui" line="37"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;img src=&quot;:/logo-LZK3.svg&quot;/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../resources/ui/donate.ui" line="41"/>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Thanks for using LibraZiK !&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;You seem to be a regular user of LibraZiK.&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;Creating and maintaining this distribution&lt;br/&gt;is a hard and constant work that takes time.&lt;br/&gt;&lt;br/&gt;That is why donations (even modest ones)&lt;br/&gt;are very much appreciated to help keep&lt;br/&gt;the motivation alive.&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;You can make a donation &lt;a href=&quot;https://liberapay.com/LibraZiK/donate&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#2980b9;&quot;&gt;here&lt;/span&gt;&lt;/a&gt; .&lt;br/&gt;More information and alternatives about&lt;br/&gt;donation can be found &lt;a href=&quot;https://librazik.tuxfamily.org/base-site-LZK/english.php#donation&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0986d3;&quot;&gt;here&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Merci d&apos;utiliser LibraZiK !&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;Il semble que vous soyez un utilisateur régulier de LibraZiK.&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;Créer et maintenir cette distribution est un&lt;br/&gt;gros et constant travail qui prend du temps.&lt;br/&gt;&lt;br/&gt;C&apos;est pourquoi les dons (mêmes modestes) sont&lt;br/&gt;fortement appréciés pour aider à garder&lt;br/&gt;la motivation.&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;Vous pouvez faire un don &lt;a href=&quot;https://liberapay.com/LibraZiK/donate&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#2980b9;&quot;&gt;ici&lt;/span&gt;&lt;/a&gt; .&lt;br/&gt;Vous trouverez de plus amples informations&lt;br/&gt;et alternatives à propos des dons &lt;a href=&quot;https://librazik.tuxfamily.org/base-site-LZK/english.php#donation&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0986d3;&quot;&gt;ici&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <location filename="../resources/ui/donate.ui" line="75"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;br/&gt;Thanks for using LibraZiK !&lt;br/&gt;Please, enjoy this little tip.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;br/&gt;Merci d&apos;utiliser LibraZiK !&lt;br/&gt;Voici une petite astuce pour vous.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../resources/ui/donate.ui" line="119"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Do you know the &lt;span style=&quot; font-weight:600;&quot;&gt;ALT+F2&lt;/span&gt; shortcut?&lt;/p&gt;&lt;p&gt;This shortcut allows you to start any software&lt;br/&gt;very quickly by simply typing its name.&lt;/p&gt;&lt;p&gt;This way you don&apos;t have to create many and&lt;br/&gt;many shortcuts on the desktop.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Connaissez-vous le raccourci &lt;span style=&quot; font-weight:600;&quot;&gt;ALT+F2&lt;/span&gt; ?&lt;/p&gt;&lt;p&gt;Ce raccourci permet de démarrer très rapidement&lt;br/&gt;n&apos;importe quel programme en tapant son nom.&lt;/p&gt;&lt;p&gt;Comme ça, vous n&apos;avez pas à créer pléthore&lt;br/&gt;de raccourcis sur votre bureau.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../resources/ui/donate.ui" line="159"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Creating and maintaining this distribution is&lt;br/&gt;a hard and constant work that takes time.&lt;br/&gt;&lt;br/&gt;That is why donations (even modest ones) are very much appreciated&lt;br/&gt;to help keep the motivation alive.&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;You can make a donation &lt;a href=&quot;https://liberapay.com/LibraZiK/donate&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#2980b9;&quot;&gt;here&lt;/span&gt;&lt;/a&gt;.&lt;br/&gt;More information and alternatives about donation can be found &lt;a href=&quot;https://librazik.tuxfamily.org/base-site-LZK/english.php#donation&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0986d3;&quot;&gt;here&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;Now, go make some music!&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Créer et maintenir cette distribution est un&lt;br/&gt;gros et constant travail qui prend du temps.&lt;br/&gt;&lt;br/&gt;C&apos;est pourquoi les dons (mêmes modestes) sont fortement appréciés&lt;br/&gt;pour aider à garder la motivation.&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;Vous pouvez faire un don &lt;a href=&quot;https://liberapay.com/LibraZiK/donate&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#2980b9;&quot;&gt;ici&lt;/span&gt;&lt;/a&gt;.&lt;br/&gt;Vous trouverez de plus amples informations et&lt;br/&gt;alternatives à propos des dons &lt;a href=&quot;https://librazik.tuxfamily.org/base-site-LZK/english.php#donation&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0986d3;&quot;&gt;ici&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;Maintenant, allez faire de la musique !&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
 </context>
 <context>
     <name>tip of day</name>
     <message>
-        <location filename="../src/lzk_donate_tip.py" line="35"/>
+        <location filename="../src/lzk_donate_tip.py" line="45"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Do you know &lt;span style=&quot; font-weight:600;&quot;&gt;ALT+F2&lt;/span&gt; shortcut ?&lt;/p&gt;&lt;p&gt;&lt;br/&gt;This shortcut allows you to start very quicky&lt;br/&gt;any software by typing its name.&lt;/p&gt;&lt;p&gt;This way, you don&apos;t have to create many and many&lt;br/&gt;shortcuts on the desktop.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Connaissez-vous le raccourci &lt;span style=&quot; font-weight:600;&quot;&gt;ALT+F2&lt;/span&gt; ?&lt;/p&gt;&lt;p&gt;&lt;br/&gt;Ce raccourci permet de démarrer très rapidement&lt;br/&gt;n&apos;importe quel programme en tapant son nom.&lt;/p&gt;&lt;p&gt;Comme ça, vous n&apos;avez pas à créer pléthore&lt;br/&gt;de raccourcis sur votre bureau.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../src/lzk_donate_tip.py" line="38"/>
+        <location filename="../src/lzk_donate_tip.py" line="48"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Do you know &lt;span style=&quot; font-weight:600;&quot;&gt;ALT+Tab&lt;/span&gt; shortcut ?&lt;/p&gt;&lt;p&gt;&lt;br/&gt;This shortcut allows you to switch quickly&lt;br/&gt;between windows.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Connaissez-vous le raccourci &lt;span style=&quot; font-weight:600;&quot;&gt;ALT+Tab&lt;/span&gt; ?&lt;/p&gt;&lt;p&gt;&lt;br/&gt;Ce raccourci vous permet de basculer rapidement&lt;br/&gt;entre les fenêtres.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
