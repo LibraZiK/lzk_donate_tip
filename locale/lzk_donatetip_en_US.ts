8c1e58695ca94539a66fdc0bfdb2f342aff98841
Olivier Humbert <trebmuh@users.noreply.github.com>
2020-10-21 18:35:11 +0200

    MàJ fichier langue EN

--- locale/lzk_donatetip_en_US.ts
+++ locale/lzk_donatetip_en_US.ts
@@ -8,46 +8,71 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../resources/ui/donate.ui" line="185"/>
+        <location filename="../resources/ui/donate.ui" line="250"/>
         <source>Do not show this message again</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../resources/ui/donate.ui" line="109"/>
+        <location filename="../resources/ui/donate.ui" line="144"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:696;&quot;&gt;Tip Of The Day&lt;br/&gt;________________&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../resources/ui/donate.ui" line="37"/>
+        <location filename="../resources/ui/donate.ui" line="40"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;img src=&quot;:/logo-LZK3.svg&quot;/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../resources/ui/donate.ui" line="75"/>
+        <location filename="../resources/ui/donate.ui" line="78"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;br/&gt;Thanks for using LibraZiK !&lt;br/&gt;Please, enjoy this little tip.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../resources/ui/donate.ui" line="119"/>
+        <location filename="../resources/ui/donate.ui" line="154"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Do you know the &lt;span style=&quot; font-weight:600;&quot;&gt;ALT+F2&lt;/span&gt; shortcut?&lt;/p&gt;&lt;p&gt;This shortcut allows you to start any software&lt;br/&gt;very quickly by simply typing its name.&lt;/p&gt;&lt;p&gt;This way you don&apos;t have to create many and&lt;br/&gt;many shortcuts on the desktop.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../resources/ui/donate.ui" line="159"/>
+        <location filename="../resources/ui/donate.ui" line="224"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Creating and maintaining this distribution is&lt;br/&gt;a hard and constant work that takes time.&lt;br/&gt;&lt;br/&gt;That is why donations (even modest ones) are very much appreciated&lt;br/&gt;to help keep the motivation alive.&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;You can make a donation &lt;a href=&quot;https://liberapay.com/LibraZiK/donate&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#2980b9;&quot;&gt;here&lt;/span&gt;&lt;/a&gt;.&lt;br/&gt;More information and alternatives about donation can be found &lt;a href=&quot;https://librazik.tuxfamily.org/base-site-LZK/english.php#donation&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0986d3;&quot;&gt;here&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;Now, go make some music!&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/ui/donate.ui" line="122"/>
+        <source>Previous Tip</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/ui/donate.ui" line="191"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/ui/donate.ui" line="188"/>
+        <source>Next Tip</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>tip of day</name>
     <message>
-        <location filename="../src/lzk_donate_tip.py" line="45"/>
+        <location filename="../src/lzk_donate_tip.py" line="38"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Do you know &lt;span style=&quot; font-weight:600;&quot;&gt;ALT+F2&lt;/span&gt; shortcut ?&lt;/p&gt;&lt;p&gt;&lt;br/&gt;This shortcut allows you to start very quicky&lt;br/&gt;any software by typing its name.&lt;/p&gt;&lt;p&gt;This way, you don&apos;t have to create many and many&lt;br/&gt;shortcuts on the desktop.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/lzk_donate_tip.py" line="48"/>
+        <location filename="../src/lzk_donate_tip.py" line="39"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Do you know &lt;span style=&quot; font-weight:600;&quot;&gt;ALT+Tab&lt;/span&gt; shortcut ?&lt;/p&gt;&lt;p&gt;&lt;br/&gt;This shortcut allows you to switch quickly&lt;br/&gt;between windows.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lzk_donate_tip.py" line="41"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Do you know &lt;span style=&quot; font-weight:600;&quot;&gt;Alt+right-click+move&lt;/span&gt; shortcut ?&lt;/p&gt;&lt;p&gt;&lt;br/&gt;This shortcut allows you to move a window. Very useful if a window is too big for your screen!&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lzk_donate_tip.py" line="42"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Do you know &lt;span style=&quot; font-weight:600;&quot;&gt;Ctrl+Alt+Esc&lt;/span&gt; shortcut ?&lt;/p&gt;&lt;p&gt;&lt;br/&gt;This shortcut allows you to kill windows if they are not responding&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
