961b33b65eb61a5acb2452d590d3e0af3d532e54
Houston4444 <picotmathieu@gmail.com>
2020-10-21 18:03:28 +0200

    Astuce précédente/suivante + messages conditionnés au DE (#8)
    * prepare the code to host previous/next
    * ajouter la fenêtre avec les précédent/suivant, j'ai fixé la taille du groupBox de l'astuce sinon c'est assez moche le groupe  qui change de taille au changement d'astuce
    * corriger un truc qui faisait que les traductions n'étaient pas reconnues
    * corriger la taille de 'Astuce du jour' pour qu'il reste bien en haut du groupe
    * ajout de tooltips sur les boutons suivant/précédent, rendre le label du logo intraduisible

--- src/lzk_donate_tip.py
+++ src/lzk_donate_tip.py
@@ -27,27 +27,63 @@ class DonateDialog(QDialog):
         self.ui = ui_donate.Ui_Dialog()
         self.ui.setupUi(self)
 
+        self.ui.toolButtonPrevious.clicked.connect(self.goPrevious)
+        self.ui.toolButtonNext.clicked.connect(self.goNext)
+
         if not isDarkTheme(self):
             self.ui.labelLogo.setText(
                 "<html><head/><body><p><img src=\":/logo-LZK3_light.svg\"/></p></body></html>")
+
+        global_tips = [
+            _translate('tip of day', "<html><head/><body><p>Do you know <span style=\" font-weight:600;\">ALT+F2</span> shortcut ?</p><p><br/>This shortcut allows you to start very quicky<br/>any software by typing its name.</p><p>This way, you don't have to create many and many<br/>shortcuts on the desktop.</p></body></html>"),
+            _translate('tip of day', "<html><head/><body><p>Do you know <span style=\" font-weight:600;\">ALT+Tab</span> shortcut ?</p><p><br/>This shortcut allows you to switch quickly<br/>between windows.</p></body></html>")]
+        
+        mate_tips = []
+        kde_tips = [_translate('tip of day', "<html><head/><body><p>Do you know <span style=\" font-weight:600;\">Ctrl+Alt+Esc</span> shortcut ?</p><p><br/>This shortcut allows you to kill windows if they are not responding</p></body></html>")]
+        
+        desktop = os.getenv('XDG_CURRENT_DESKTOP')
+        if desktop == 'MATE':
+            desktop_tips = mate_tips
+        elif desktop == 'KDE':
+            desktop_tips = kde_tips
+
+        # mixer les messages généraux et les messages d'environnement de bureau
+        self.tips = []
+        n = 0
+        while global_tips or desktop_tips:
+            if not global_tips:
+                self.tips += desktop_tips
+                break
+            if not desktop_tips:
+                self.tips += global_tips
+                break
+
+            if n % 2:
+                self.tips.append(desktop_tips.pop(0))
+            else:
+                self.tips.append(global_tips.pop(0))
+            n += 1
+        
+        self.tip_day = 0
     
     def notAgainChecked(self):
         return bool(self.ui.checkBox.checkState())
     
     def setTipOfTheDay(self, day):
-        message = _translate('tip of day', "<html><head/><body><p>Do you know <span style=\" font-weight:600;\">ALT+F2</span> shortcut ?</p><p><br/>This shortcut allows you to start very quicky<br/>any software by typing its name.</p><p>This way, you don't have to create many and many<br/>shortcuts on the desktop.</p></body></html>")
-        
-        # la valeur après le modulo doit être égale au nombre de messages
-        tip_day = day % 2
-        
-        if tip_day == 0:
-            # ça ça me paraît pertinent
-            message = _translate('tip of day', "<html><head/><body><p>Do you know <span style=\" font-weight:600;\">ALT+F2</span> shortcut ?</p><p><br/>This shortcut allows you to start very quicky<br/>any software by typing its name.</p><p>This way, you don't have to create many and many<br/>shortcuts on the desktop.</p></body></html>")
-        elif tip_day == 1:
-            # ça c'est un peu naze, c'est juste pour l'exemple
-            message = _translate('tip of day', "<html><head/><body><p>Do you know <span style=\" font-weight:600;\">ALT+Tab</span> shortcut ?</p><p><br/>This shortcut allows you to switch quickly<br/>between windows.</p></body></html>")
-            
+        self.tip_day = day % len(self.tips)
+        message = self.tips[self.tip_day]
+
         self.ui.labelTipOfTheDay.setText(message)
+    
+    def goPrevious(self):
+        day = len(self.tips) - 1
+        if self.tip_day:
+            day = self.tip_day -1
+
+        self.setTipOfTheDay(day)
+        
+    def goNext(self):
+        self.setTipOfTheDay(self.tip_day + 1)
 
 if __name__ == "__main__":
     force = False
