b847cb5dc9e42ad2ddc54ce9ca91bf1c9a4f24f9
Olivier Humbert <trebmuh@users.noreply.github.com>
2020-10-31 22:19:07 +0100

    Update lzk_donatetip_fr.ts

--- locale/lzk_donatetip_fr.ts
+++ locale/lzk_donatetip_fr.ts
@@ -70,12 +70,12 @@
 <context>
     <name>bottom_texts</name>
     <message>
-        <location filename="../src/librazik_tips.py" line="54"/>
+        <location filename="../src/librazik_tips.py" line="53"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Creating and maintaining this distribution is&lt;br/&gt;a hard and constant work that takes time.&lt;br/&gt;&lt;br/&gt;That is why donations (even modest ones) are very much appreciated&lt;br/&gt;to help keep the motivation alive.&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;You can make a donation &lt;a href=&quot;https://liberapay.com/LibraZiK/donate&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#2980b9;&quot;&gt;here&lt;/span&gt;&lt;/a&gt;.&lt;br/&gt;More information and alternatives about donation can be found &lt;a href=&quot;https://librazik.tuxfamily.org/base-site-LZK/english.php#donation&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0986d3;&quot;&gt;here&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;Now, go make some music!&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Créer et maintenir cette distribution est un&lt;br/&gt;gros et constant travail qui prend du temps.&lt;br/&gt;&lt;br/&gt;C&apos;est pourquoi les dons (mêmes modestes) sont fortement appréciés&lt;br/&gt;pour aider à garder la motivation.&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;Vous pouvez faire un don &lt;a href=&quot;https://liberapay.com/LibraZiK/donate&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#2980b9;&quot;&gt;ici&lt;/span&gt;&lt;/a&gt;.&lt;br/&gt;Vous trouverez de plus amples informations et&lt;br/&gt;alternatives à propos des dons &lt;a href=&quot;https://librazik.tuxfamily.org/base-site-LZK/don.php&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0986d3;&quot;&gt;ici&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;Maintenant, allez faire de la musique !&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../src/librazik_tips.py" line="55"/>
+        <location filename="../src/librazik_tips.py" line="54"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;LibraZiK takes many hours each week to be what it is.&lt;br/&gt;LibraZiK is free (as a free speech), but not really free (as a free drink).&lt;br/&gt;If you don&apos;t donate, the project will eventually stop.&lt;br/&gt;So, if you&apos;re able to finance the project, please donate!&lt;/br&gt;If you&apos;re not able, that&apos;s ok, keep using it, we don&apos;t want money to be a blocker.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;LibraZiK prend de nombreuses heures chaque semaine pour être ce que c&apos;est.&lt;br/&gt;LibraZiK est libre, mais pas vraiment gratuit.&lt;br/&gt;Si vous ne faites pas de dons, le projet finira par s&apos;arrêter.&lt;br/&gt;Alors, si vous êtes en mesure de financer le projet, faites un don !&lt;/br&gt;Si vous n&apos;en êtes pas capable, c&apos;est bon, continuez à l&apos;utiliser, nous ne voulons pas que l&apos;argent soit un bloqueur.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
@@ -93,12 +93,12 @@
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Connaissez-vous le raccourci &lt;span style=&quot; font-weight:600;&quot;&gt;ALT + Tab&lt;/span&gt; ?&lt;/p&gt;&lt;p&gt;&lt;br/&gt;Ce raccourci vous permet de basculer rapidement&lt;br/&gt;entre les fenêtres.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../src/librazik_tips.py" line="50"/>
+        <location filename="../src/librazik_tips.py" line="49"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Do you know &lt;span style=&quot; font-weight:600;&quot;&gt;Alt + left-click + move&lt;/span&gt; shortcut ?&lt;/p&gt;&lt;p&gt;&lt;br/&gt;This shortcut allows you to move a window. Very useful if a window is too big for your screen!&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Connaissez-vous le raccourci &lt;span style=&quot; font-weight:600;&quot;&gt;Alt + clic-gauche + déplacement&lt;/span&gt; ?&lt;/p&gt;&lt;p&gt;&lt;br/&gt;Ce raccourci vous permet de déplacer une fenêtre. Très utile si une fenêtre est trop grande pour votre écran !&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../src/librazik_tips.py" line="49"/>
+        <location filename="../src/librazik_tips.py" line="48"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Do you know &lt;span style=&quot; font-weight:600;&quot;&gt;Ctrl + Alt + Esc&lt;/span&gt; shortcut ?&lt;/p&gt;&lt;p&gt;&lt;br/&gt;This shortcut allows you to kill windows if they are not responding&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Connaissez-vous le raccourci &lt;span style=&quot; font-weight:600;&quot;&gt;Ctrl + Alt + Esc&lt;/span&gt; ?&lt;/p&gt;&lt;p&gt;&lt;br/&gt;Ce raccourci vous permet de tuer les fenêtres si elles ne répondent plus.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
@@ -109,8 +109,8 @@
     </message>
     <message>
         <location filename="../src/librazik_tips.py" line="41"/>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Do you know that you can come and chat live with other LibraZiK users?&lt;/p&gt;&lt;p&gt;&lt;/br&gt;Come on the &lt;span style=&quot; font-weight:600;&quot;&gt;#librazik IRC channel.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Savez-vous que vous pouvez venir discuter en direct avec d&apos;autres utilisateurs de LibraZiK ?&lt;/p&gt;&lt;p&gt;&lt;/br&gt;Venez sur le &lt;span style=&quot; font-weight:600;&quot;&gt;canal IRC #librazik.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Do you know that you can come and chat live with other LibraZiK users?&lt;/p&gt;&lt;p&gt;&lt;/br&gt;Come on the &lt;span style=&quot; font-weight:600;&quot;&gt;#librazik IRC channel.&lt;/p&gt;&lt;p&gt;More information about that &lt;a href=&quot;https://librazik.tuxfamily.org/base-site-LZK/english.php#help&quot;&gt;&lt;span style=&quot; font-weight:600; text-decoration: underline; color:#2980b9;&quot;&gt;here&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Savez-vous que vous pouvez venir discuter en direct avec d&apos;autres utilisateurs de LibraZiK ?&lt;/p&gt;&lt;p&gt;&lt;/br&gt;Venez sur le &lt;span style=&quot; font-weight:600;&quot;&gt;canal IRC #librazik.&lt;/p&gt;&lt;p&gt;Plus d&apos;informations à ce sujet &lt;a href=&quot;https://librazik.tuxfamily.org/base-site-LZK/contact.php#IRC&quot;&gt;&lt;span style=&quot; font-weight:600; text-decoration: underline; color:#2980b9;&quot;&gt;ici&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
 </context>
 </TS>
